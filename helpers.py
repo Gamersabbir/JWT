2ac1b38eaeddc9b00c80a24cca6a5b3013148a5b
! ＧＡＭＥＲ ＳＡＢＢＩＲ <sabbirallvideo@gmail.com>
2025-10-17 19:56:08 +0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -11,14 +11,17 @@ def pad(text: bytes) -> bytes:
     padding_length = AES.block_size - (len(text) % AES.block_size)
     return text + bytes([padding_length] * padding_length)
 
+
 def aes_cbc_encrypt(key: bytes, iv: bytes, plaintext: bytes) -> bytes:
     aes = AES.new(key, AES.MODE_CBC, iv)
     return aes.encrypt(pad(plaintext))
+
 
 def decode_protobuf(encoded_data: bytes, message_type: message.Message) -> message.Message:
     instance = message_type()
     instance.ParseFromString(encoded_data)
     return instance
+
 
 def json_to_proto(json_data: str, proto_message: Message) -> bytes:
     json_format.ParseDict(json.loads(json_data), proto_message)
@@ -26,6 +29,4 @@ def json_to_proto(json_data: str, proto_message: Message) -> bytes:
 
 
 def jwt_decode(token):
-    return jwt.decode(jwt=token,options={"verify_signature":False},algorithms=['HS256'],verify=False)
-
-
+    return jwt.decode(jwt=token, options={"verify_signature": False}, algorithms=["HS256"], verify=False)
